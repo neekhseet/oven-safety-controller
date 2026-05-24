14ebb0aa99a604b85c87762b9ec66f3c117888f7
alex <iteeshkeen@gmail.com>
2026-05-24 19:09:34 +0500

    test: heater must be disabled if temperature more than target

--- tests/test_osc.cpp
+++ tests/test_osc.cpp
@@ -8,3 +8,13 @@ TEST(ControllerTests, SetAndGetTargetTemperature) {
   controller.setTargetTemperature(10);
   EXPECT_EQ(controller.getTargetTemperature(), 10);
 }
+
+TEST(ControllerTest, HeaterDisableIfTempMoreTarget) {
+  MockSensor sensor;
+  EXPECT_CALL(sensor, readTemperature()).willOnce(Return(150.9));
+
+  Controller controller(sensor);
+  controller.setTargetTemperature(100.0);
+  controller.update();
+  EXPECT_FALSE(controller.isHeaterOn());
+}
