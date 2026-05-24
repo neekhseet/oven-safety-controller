d13d8ef48766bdc12e186666afb74467ed3aa363
alex <iteeshkeen@gmail.com>
2026-05-24 18:52:26 +0500

    feat: add set and get target temperature function

--- includes/controller.h
+++ includes/controller.h
@@ -2,9 +2,13 @@
 #define CONTROLLER_H
 
 class Controller {
+  double targetTemp{0};
 
 public:
   Controller() = default;
+
+  void setTargetTemperature(double temp) { targetTemp = temp; };
+  double getTargetTemperature() { return targetTemp; }
 };
 
 #endif
