17baf16605fa58bcbd2e3e0558b85320fd3d25f9
alex <iteeshkeen@gmail.com>
2026-05-24 18:49:49 +0500

    test: set and get target temperature for controller

--- tests/test_osc.cpp
+++ tests/test_osc.cpp
@@ -2,3 +2,9 @@
 #include <gtest/gtest.h>
 
 TEST(ControllerTests, CanBeCreated) { Controller controller; }
+
+TEST(ControllerTests, SetAndGetTargetTemperature) {
+  Controller controller;
+  controller.setTargetTemperature(10);
+  EXPECT_EQ(controller.getTargetTemperature(), 10)
+}
