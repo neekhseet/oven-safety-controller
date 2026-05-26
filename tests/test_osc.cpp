a379e05f9cfb16324804243c479d78f050cd7101
alex <iteeshkeen@gmail.com>
2026-05-26 19:14:34 +0500

    test: heater can be enabled after alarm reset

--- tests/test_osc.cpp
+++ tests/test_osc.cpp
@@ -50,3 +50,19 @@ TEST(ControllerTests, HeaterCantWorkWhileAlarmEnabled) {
   controller.enableHeater();
   EXPECT_FALSE(controller.isHeaterOn());
 }
+
+TEST(ControllerTests, HeaterCanBeEnabledAfterAlarmReset) {
+  MockSensor sensor;
+  EXPECT_CALL(sensor, readTemperature()).WillOnce(Return(150.9));
+
+  Controller controller(sensor);
+  controller.setTargetTemperature(100.0);
+  controller.update();
+
+  EXPECT_TRUE(controller.isAlarmOn());
+  EXPECT_CALL(sensor, readTemperature()).WillOnce(Return(90.9));
+  controller.update();
+  controller.resetAlarm();
+  controller.enableHeater();
+  EXPECT_FALSE(controller.isHeaterOn());
+}
