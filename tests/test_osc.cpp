342d30bea1df226fce325d5c82d51ef948a89451
alex <iteeshkeen@gmail.com>
2026-05-26 19:10:48 +0500

    test: heater cant be enabled while alarm

--- tests/test_osc.cpp
+++ tests/test_osc.cpp
@@ -35,3 +35,18 @@ TEST(ControllerTests, AlarmOnTargetTemp) {
 
   EXPECT_TRUE(controller.isAlarmOn());
 }
+
+TEST(ControllerTests, HeaterCantWorkWhileAlarmEnabled) {
+  MockSensor sensor;
+  EXPECT_CALL(sensor, readTemperature()).WillOnce(Return(150.9));
+
+  Controller controller(sensor);
+  controller.setTargetTemperature(100.0);
+  controller.update();
+
+  EXPECT_TRUE(controller.isAlarmOn());
+  EXPECT_CALL(sensor, readTemperature()).WillOnce(Return(90.9));
+  controller.update();
+  controller.enableHeater();
+  EXPECT_FALSE(controller.isHeaterOn());
+}
